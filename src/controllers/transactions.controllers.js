728e5462e83cf345c9982e5bf572b76183e4baba
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-11-22 16:45:16 -0300

    build: removido money.utils.js

--- src/controllers/transactions.controllers.js
+++ src/controllers/transactions.controllers.js
@@ -1,11 +1,11 @@
 import db from '../db/connection.js';
-import { convertMoneyToInteger } from '../utils/money.utils.js'
 
 async function createNewTransaction(req, res) {
     const { userId, transactionData } = res.locals;
 
     try {
-        transactionData.value = convertMoneyToInteger(transactionData.value);
+        transactionData.value = transactionData.value * 100;
+
         const newTransaction = {
             ...transactionData,
             userId,
