b2af41b8fbb17a6ab1da91afb47a8f762223c662
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-11-22 15:23:47 -0300

    feat: criado controller getTransactions
    essa controller buscar no banco transacoes onde tenham o userId igual o passado pela controller verifyToken e retona para o cliente as transacoes enconradas

--- src/controllers/transactions.controllers.js
+++ src/controllers/transactions.controllers.js
@@ -18,7 +18,23 @@ async function createNewTransaction(req, res) {
     }
 }
 
+async function getTransactions(req, res) {
+    const { userId } = res.locals;
+
+    {
+        try {
+            console.log(userId)
+            const transactions = await db.collection('transactions').find({ userId }).toArray();
+
+            return res.status(200).send(transactions);
+        } catch (error) {
+            return res.status(500).send({ message: 'Erro interno do servidor.' });
+        }
+    }
+}
+
 export {
     createNewTransaction,
+    getTransactions,
 
 }
--- src/routers/transactions.routers.js
+++ src/routers/transactions.routers.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middlewares/auth.middlewares.js";
 import { verifyTransaction } from "../middlewares/transactions.middlewares.js";
-import { createNewTransaction } from "../controllers/transactions.controllers.js";
+import { createNewTransaction, getTransactions } from "../controllers/transactions.controllers.js";
 
 const router = express.Router();
 
 router.use(verifyToken);
 
 router.post('/transaction', verifyTransaction, createNewTransaction);
+router.get('/transaction', getTransactions)
 
 export default router;
