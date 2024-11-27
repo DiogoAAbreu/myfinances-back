ba594b041bea19e7bc277562b16f6788ed64e9f6
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-11-27 07:49:58 -0300

    style: adicionado ; onde nao tinha no arquivo index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import authRouters from './routers/auth.routers.js'
-import transactionRouters from './routers/transactions.routers.js'
-import usersRouters from './routers/users.routers.js'
+import authRouters from './routers/auth.routers.js';
+import transactionRouters from './routers/transactions.routers.js';
+import usersRouters from './routers/users.routers.js';
 
-const app = express()
+const app = express();
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(authRouters);
 app.use(usersRouters);
@@ -17,4 +17,4 @@ app.use(transactionRouters);
 
 app.listen(5000, () => {
     console.log('Run in http://localhost:5000/')
-})
+});
